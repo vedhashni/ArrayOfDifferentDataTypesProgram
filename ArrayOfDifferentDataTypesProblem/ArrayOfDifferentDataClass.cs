9314d1cefa11515cb209165f31b2761fb943f13f
[vedhashni] <[vedhavel.1234@gmail.com]>
2021-07-08 21:08:05 +0530

    [Vedhashni] Updated the file with different types of array

--- ArrayOfDifferentDataTypesProblem/ArrayOfDifferentDataClass.cs
+++ ArrayOfDifferentDataTypesProblem/ArrayOfDifferentDataClass.cs
@@ -8,5 +8,43 @@ namespace ArrayOfDifferentDataTypesProblem
 {
     class ArrayOfDifferentDataClass
     {
-    }
+        // To print integer array
+        public static void ToPrintIntArray(int[] intarr)
+        {
+            foreach (var i in intarr)
+            {
+                Console.Write(i + " ");
+            }
+
+        }
+        //To print float array
+        public static void ToPrintFloatArray(float[] floatarr)
+        {
+            foreach (var j in floatarr)
+            {
+                Console.Write(j + " ");
+            }
+
+        }
+        //To print char array
+        public static void ToPrintCharArray(char[] chararr)
+        {
+            foreach (var k in chararr)
+            {
+                Console.Write(k + " ");
+            }
+
+        }
+            //To print the string array
+
+            public static void ToPrintStringArray(String[] stringarr)
+            {
+                foreach (var l in stringarr)
+                {
+                    Console.Write(l + " ");
+                }
+
+            }
+
+        }
 }
--- ArrayOfDifferentDataTypesProblem/Program.cs
+++ ArrayOfDifferentDataTypesProblem/Program.cs
@@ -11,6 +11,41 @@ namespace ArrayOfDifferentDataTypesProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to ArrayOfDifferentDataTypes Problem");
+            //Initializing the IntArray
+            int[] intarr = { 20, 40, 50, 60 };
+            //Intializing the FloatArray
+            float[] floatarr = { 2.3f, 6.8f, 9.0f, 8.3f };
+            //Initializing the charArray
+            char[] chararr = { 'W', 'E', 'L', 'C', 'O', 'M', 'E' };
+            //Initializing the StringArray
+            String[] stringarr = { "Apple", "Orange", "Watermelon", "Mango" };
+
+            Console.WriteLine("1.To print int array\n 2.To print float array\n 3.To print char array\n 4.To print string array");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    ArrayOfDifferentDataClass.ToPrintIntArray(intarr);
+                    break;
+
+                case 2:
+                    ArrayOfDifferentDataClass.ToPrintFloatArray(floatarr);
+                    break;
+
+                case 3:
+                    ArrayOfDifferentDataClass.ToPrintCharArray(chararr);
+                    break;
+
+                case 4:
+                    ArrayOfDifferentDataClass.ToPrintStringArray(stringarr);
+                    break;
+
+                case 5:
+                    return;
+
+                default:
+                    break;
+            }
         }
     }
 }
